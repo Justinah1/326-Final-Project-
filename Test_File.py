62f6883c118d598af1b7335d1d8ea3e5ee9cf940
Tolulope Bashua <bashuatolu@gmail.com>
2021-10-27 11:17:01 -0400

    tolu's changes

--- Test_File.py
+++ Test_File.py
@@ -1,6 +1,6 @@
 """A test python file that adds two number"""
-def addnum(x, y, a):
-    z = x + y + a
+def addnum(x, y, a, c):
+    z = x + y + a - c
     return z
 
-print("test Yanni")
+print("test Tolu")
