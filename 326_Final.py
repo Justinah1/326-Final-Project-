7b685a40df7b6268f5493f94ae87183d340bd97c
Tony Vo <votony33@gmail.com>
2021-12-11 15:04:22 -0500

    tony changes

--- 326_Final.py
+++ 326_Final.py
@@ -21,62 +21,63 @@ class Spar:
     
     def deck(self):
         """This method creates and sets the deck of cards 
-
         Returns:
         deck [str]: returns all deck card values
     """
     
         face = ["K","Q","J","A", "6", "7", "8", "9", "10"]
         suits = ["Hearts","Spades","Clubs","Diamonds"]
-        self.deck = {}
+        self.deck = []
         for i in face:
             for x in suits:
                 if x is not "Spades" and i is not "A":
-                    self.deck[x] = i 
+                    self.deck.append(Card(i, x))
         return self.deck 
         
     def deal(self):
         shuffledDeck = random.shuffle(self.deck())
-        cards =[]
         
         for player in self.playersList:
             amountOfCardsPerTrick = 1
             while amountOfCardsPerTrick <= 5:
-                x = shuffledDeck
-                cards.append(x)
+                player.cards.append(shuffledDeck.pop())
                 amountOfCardsPerTrick += 1  
+                
+    def setCurrCard(self, player):
+        self.currCard = player.playTurn()
             
     def game(self):
         turn = -1
+        round = 0
         player = None
-        while len(self.playersList[0].cards) > 0:
+        
+        if round == 0:
+            self.setCurrCard(self.playersList[0])
+        
+        while round <= 5:
             turn += 1
             player = self.playersList[turn % len(self.playersList)]
-            if 
+            if len(self.playersList[0].cards) > 0:
+                player.playTurn() 
         
-            
+class Card:
+    def __init(self, face, suit):
+        self.suit = suit
+        self.face = face       
+    
+    def __repr__(self):
+        print(f"Card({self.suit},{self.face}")     
             
 class Player:
-    def __init__(self, name, cards = []):
+    def __init__(self, name, currCard, cards = []):
         self.name = name
         self.cards = cards
+        self.currCard = currCard
         
-    def playTurn(self,player):
-        if self.currCard == None:
-            print()
-        while True:
-            try:
-                player_input = int(input(f"Enter a suit between  {p1}:"))
-                if player_input < 20 or player_input > 30:
-                    print("Out of range")
-                else:
-                    p1_num = player_input + randrange(10)
-                    print(p1_num)
-                if p1_num > p2_num:
-                    print("p1 wins")
-                elif p2_num > p1_num:
-                    print("p2 wins")
-                else:
-                    print("It's a tie")
-            except ValueError:
-                print("Not a valid entry")
+    def playTurn(self):
+       
+        for card in self.cards:
+            print(card)
+        first_card = int(input("Select a card (Using a number)"))
+        self.currCard = self.cards.pop(first_card - 1)
+        return self.currCard
